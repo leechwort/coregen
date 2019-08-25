2bed10061ce55ac5d52ac95d4d8a5f446ecb6876
Artem Synytsyn <sincoon@gmail.com>
2019-08-25 21:01:56 +0300

    Added comments with frequencies to generated files

--- wavegen.py
+++ wavegen.py
@@ -4,7 +4,7 @@ from scipy import fftpack
 
 # Create a signal generator.
 sg = sig.SigGen()
-
+Fs = 44100
 # Create a wave table with 1024 slots to store the waves.
 wt = wavetable.WaveTable(1, wave_len=512)
 m = sg.saw()
@@ -12,15 +12,18 @@ wt.waves = [m]
 
 #visualize.plot_wavetable(wt)
 
+timestep = 1.0 / Fs
 X = fftpack.rfft(wt.waves[0])
-
+freq = fftpack.rfftfreq(X.size, d=timestep)
 # 
 # Make array uniform with C input data
 X = np.insert(X, 0, 0)
+freq = np.insert(freq, 0, 0)
 X = np.delete(X, -1)
+freq = np.delete(freq, -1)
 
 outfile = "./waves/sawtooth_512.cgw"
 with open(outfile, 'w') as f:
     f.write(str(len(X)) + '\n')
-    for data in X:    
-        f.write(str(data) + '\n')
+    for i, data in enumerate(X):    
+        f.write(str(data) + '\t\t/* ' + str(freq[i]) + ' Hz  */\n')
